1dc411aa6c8c39fc63906553f7a38cb45fea6607
Alexander <aguillenhernandez95@gmail.com>
2023-03-07 15:13:43 +0100

    Add errorHandler middleware function

---
+++ src/server/middlewares/errorHandler/errorHandler.ts
@@ -0,0 +1,19 @@
+import "../../../loadEnvironment.js";
+import { type NextFunction, type Request, type Response } from "express";
+import createDebug from "debug";
+import type CustomError from "../../../CustomError/CustomError";
+
+const debug = createDebug("lingodeck:src:server:middlewares:errorHandler");
+
+const errorHandler = (
+  error: CustomError,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  debug(error.message);
+
+  response
+    .status(error.statusCode || 500)
+    .json({ error: error.publicMessage || "Something went wrong" });
+};
