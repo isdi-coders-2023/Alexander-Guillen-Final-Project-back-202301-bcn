4e05eaba39b66a3dcc61a11d06d270484ad6539e
Alexander <aguillenhernandez95@gmail.com>
2023-03-07 15:54:25 +0100

    Add endpointNotFound middleware

--- src/server/middlewares/errorHandler/errorHandler.ts
+++ src/server/middlewares/errorHandler/errorHandler.ts
@@ -1,7 +1,7 @@
 import "../../../loadEnvironment.js";
 import { type NextFunction, type Request, type Response } from "express";
 import createDebug from "debug";
-import type CustomError from "../../../CustomError/CustomError";
+import type CustomError from "../../../CustomError/CustomError.js";
 
 const debug = createDebug("lingodeck:src:server:middlewares:errorHandler");
 
