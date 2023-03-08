59c41fe76ef86d9db89371da9a4a6bb583881341
Alexander <aguillenhernandez95@gmail.com>
2023-03-08 18:12:44 +0100

    Modify userCredentialsValidation request type

--- src/server/middlewares/validations/userCredentialsValidation/userCredentialsValidation.test.ts
+++ src/server/middlewares/validations/userCredentialsValidation/userCredentialsValidation.test.ts
@@ -16,11 +16,19 @@ describe("Given an userCredentialsValidation middleware", () => {
       request.body = {
         username: "aguillen",
         password: "user1",
-      } as UserCredentials;
+      };
       const publicMessage =
         '"password" length must be at least 8 characters long';
 
-      userCredentialsValidation(request as Request, response as Response, next);
+      userCredentialsValidation(
+        request as Request<
+          Record<string, unknown>,
+          Record<string, unknown>,
+          UserCredentials
+        >,
+        response as Response,
+        next
+      );
 
       expect(next.mock.calls[0][0]).toHaveProperty("message", message);
       expect(next.mock.calls[0][0]).toHaveProperty("statusCode", status);
@@ -36,11 +44,19 @@ describe("Given an userCredentialsValidation middleware", () => {
       request.body = {
         username: "-90-A-123(409)",
         password: "alexanderGH123",
-      } as UserCredentials;
+      };
       const publicMessage =
         '"username" must only contain alpha-numeric characters';
 
-      userCredentialsValidation(request as Request, response as Response, next);
+      userCredentialsValidation(
+        request as Request<
+          Record<string, unknown>,
+          Record<string, unknown>,
+          UserCredentials
+        >,
+        response as Response,
+        next
+      );
 
       expect(next.mock.calls[0][0]).toHaveProperty("message", message);
       expect(next.mock.calls[0][0]).toHaveProperty("statusCode", status);
