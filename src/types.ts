a5be544173b77b9fd7e9c38b44356b0b9562ab26
Alexander <aguillenhernandez95@gmail.com>
2023-03-08 10:48:20 +0100

    Test loginUser controller

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,9 @@ export interface ResponseError {
   error: string;
 }
 
+export interface LoginResponse {
+  token: string;
+}
 export interface UserCredentials {
   username: string;
   password: string;
